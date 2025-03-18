b18fe272c2f89b62903af51b2e2baa0368bd9361
IreneMocnik <iremocnik@gmail.com>
2025-03-18 10:31:23 -0300

    Actualización tp2

--- tp2.py
+++ tp2.py
@@ -8,26 +8,65 @@ Created on Sun Mar 16 23:03:53 2025
 import numpy as np
 import matplotlib.pyplot as plt
 
-r=5.00
-c=0.000002
-l=0.5
-wf=300.00
+Q = 100
+w0= 1
+# se analiza un intervalo dos décadas antes y después de la frecuencia de corte. (w/100 hasta w0*100)
+w= np.logspace(np.log10(w0/100), np.log10(100*w0), 10000)
 
-def funciona(r,c,l):
-    w = np.arange(start = 0, stop = wf, step = 20)
-    Ta = w*c*r/(np.sqrt(w*w*r*r*c*c+(1-l*c*w*w)*(1-l*c*w*w)))
+def funciona(Q, w0):
+    w = np.logspace(np.log10(w0/100), np.log10(100*w0), 10000)
+    Ta = (w / (w0 * Q)) / np.sqrt((w / w0)**2 * Q**2 + (1 - (w / w0)**2)**2)
     return w, Ta
-w, Ta =funciona(r, c, l)
-plt.figure(1)
-plt.plot(w, 20*np.log10(Ta), label="Magnitud")
+def fasea(Q,w0):
+    w = np.logspace(np.log10(w0/100), np.log10(100*w0), 10000)
+    fa = np.arctan((1 - (w / w0)**2) / ((w / w0) * Q))
+    return w, fa
 
-def funcionb(r,c,l):
-    w = np.arange(start = 0, stop = wf, step = 20)
-    Tb = w*c*r*l/(np.sqrt(w*w*(c*r*l + l)*(c*r*l + l) + r*r))
+def funcionb(Q, w0):
+    w = np.logspace(np.log10(w0/100), np.log10(100*w0), 10000)
+    Tb = (1 / (Q * w0)) * (w / np.sqrt((w * (1 + 1 / (Q * w0)))**2 + (w0 / Q)**2))
     return w, Tb
-w, Tb =funcionb(r, c, l)
+def faseb(Q,w0):
+    w = np.logspace(np.log10(w0/100), np.log10(100*w0), 10000)
+    fb = np.arctan(-w * Q * (1 + 1 / (Q * w0)) / w0)
+    return w, fb
+
+
+w, Ta =funciona(Q, w0)
+w, fa =fasea(Q,w0)
+w, Tb =funcionb(Q, w0)
+w, fb =faseb(Q,w0)
+
+
+plt.figure(1)
+plt.semilogx(w, 20*np.log10(Ta), label="Magnitud")
+plt.xlabel("Frecuencia (rad/s)")
+plt.ylabel("Ganancia (dB)")
+plt.title("Diagrama de Bode - Magnitud A")
+plt.grid()
+
 plt.figure(2)
-plt.plot(w, 20*np.log10(Tb), label="Magnitud")
+plt.semilogx(w, fa)  
+plt.ylabel("Fase (radianes)")
+plt.xlabel("Frecuencia (rad/s)")
+plt.title("Diagrama de Bode - Fase A")
+plt.grid()
+
+plt.figure(3)
+plt.semilogx(w, 20*np.log10(Tb), label="Magnitud")
+plt.xlabel("Frecuencia (rad/s)")
+plt.ylabel("Ganancia (dB)")
+plt.title("Diagrama de Bode - Magnitud B")
+plt.grid()
+
+plt.figure(4)
+plt.semilogx(w, fb)  
+plt.ylabel("Fase (radianes)")
+plt.xlabel("Frecuencia (rad/s)")
+plt.title("Diagrama de Bode - Fase B")
+plt.grid()
+
+plt.show()
 
 
 
